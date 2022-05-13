71894b5f1c3aa3a80d97f1508c5f8386c23ff752
sansay500 <grau2Kit>
2022-05-13 13:25:38 +0300

    All done

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -10,12 +10,12 @@ use Orchid\Screen\AsSource;
 
 class Offer extends Model
 {
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
    use HasFactory, AsSource, Filterable, Attachable;
 
    protected $fillable=['offer_content','offer_hotel',
        'offer_nights','offer_rooms_quantity','offer_room_class','offer_meals','offer_arrival_date',
        'offer_price','user_id','offer_city'];
-
 
    public function user(){
        return $this->belongsTo(User::class);
--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
+    use \Backpack\CRUD\app\Models\Traits\CrudTrait;
     use HasFactory;
 
     protected $fillable=['hotel','seller_id','buyer_id',
